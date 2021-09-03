8353ce6df2b8f047178b76ce9499cb1398d87323
Pascal SUN <pascal.sun@protonmail.com>
2021-09-03 14:40:31 +0200

    add paragraph for more information

--- popup/script.js
+++ popup/script.js
@@ -64,10 +64,20 @@ gettingStoredData.then(results => {
             addElement("currentLinkTag", message, "incorrect");
         }
     }
-    console.log(counter);
+    if (counter === 0) {
+        message = "There are no <i>&lt;link&gt; tags</i> with the <i>href attribute</i> on the current page."
+    } else {
+        message = "There are <b>" + counter + "</b> <i>&lt;link&gt; tags</i> with the <i>href attribute</i> on the current page:"
+    }
+    document.getElementById("currentLinkTag-p").innerHTML = message;
+
+
+
     // Script tag sort
     data = results[results.currentHostname]["scriptTags"][results.currentPathname];
+    counter = 0;
     for (let i in data) {
+        counter += 1;
         url = new URL(i);
         let correct = false;
         if (url.hostname === results.currentHostname) {
@@ -87,9 +97,18 @@ gettingStoredData.then(results => {
             addElement("currentScriptTag", message, "incorrect");
         }
     }
+    if (counter === 0) {
+        message = "There are no <i>&lt;script&gt; tags</i> with the <i>src attribute</i> on the current page."
+    } else {
+        message = "There are <b>" + counter + "</b> <i>&lt;script&gt; tags</i> with the <i>src attribute</i> on the current page:"
+    }
+    document.getElementById("currentScriptTag-p").innerHTML = message;
 
     // All tag sort
     data = results[results.currentHostname]["scriptTags"]["allURLs"];
+    let counterCorrectwoSRI = 0;
+    let counterCorrectwithSRI = 0;
+    let counterIncorrect = 0;
     for (let i in data) {
         let SRI = false;
         message = '<script src="' + i + '" ';
@@ -105,12 +124,16 @@ gettingStoredData.then(results => {
         url = new URL(i);
         if (url.hostname == results.currentHostname) {
             addElement("hostnameCorrectwoSRI", message, "correct");
+            counterCorrectwoSRI += 1;
         } else if (SRI === true) {
             addElement("hostnameCorrectwithSRI", message, "correct");
+            counterCorrectwithSRI += 1;
         } else {
             addElement("hostnameIncorrect", message, "incorrect");
+            counterIncorrect += 1;
         }
     }
+
     // All tag sort
     data = results[results.currentHostname]["linkTags"]["allURLs"];
     for (let i in data) {
@@ -128,12 +151,34 @@ gettingStoredData.then(results => {
         url = new URL(i);
         if (url.hostname === results.currentHostname) {
             addElement("hostnameCorrectwoSRI", message, "correct");
+            counterCorrectwoSRI += 1;
         } else if (SRI === true) {
             addElement("hostnameCorrectwithSRI", message, "correct");
+            counterCorrectwithSRI += 1;
         } else {
             addElement("hostnameIncorrect", message, "incorrect");
+            counterIncorrect += 1;
         }
     }
+    if (counterCorrectwoSRI === 0) {
+        message = "There are no <i>&lt;link&gt;</i> or <i>&lt;script&gt; tags</i> with a relative URL in the <i>href</i> or <i>src attribute</i> on <b>" + results.currentHostname +"</b>.";
+    } else {
+        message = "There are <b>" + counterCorrectwoSRI + "</b> <i>&lt;link&gt;</i> or <i>&lt;script&gt; tags</i> with a relative URL in the <i>href</i> or <i>src attribute</i> on <b>" + results.currentHostname + "</b>:";
+    }
+    document.getElementById("hostnameCorrectwoSRI-p").innerHTML = message;
+
+    if (counterCorrectwithSRI === 0) {
+        message = "There are no <i>&lt;link&gt;</i> or <i>&lt;script&gt; tags</i> that have the <i>integrity attribute</i> on <b>" + results.currentHostname +"</b>.";
+    } else {
+        message = "There are <b>" + counterCorrectwithSRI + "</b> <i>&lt;link&gt;</i> or <i>&lt;script&gt; tags</i> that have the <i>integrity attribute</i> on <b>" + results.currentHostname + "</b>:";
+    }
+    document.getElementById("hostnameCorrectwithSRI-p").innerHTML = message;
+    if (counterIncorrect === 0) {
+        message = "There are no <i>&lt;link&gt;</i> or <i>&lt;script&gt; tags</i> that require the <i>integrity attribute</i> on <b>" + results.currentHostname +"</b>.";
+    } else {
+    message = "There are <b>" + counterIncorrect + "</b> <i>&lt;link&gt;</i> or <i>&lt;script&gt; tags</i> that require the <i>integrity attribute</i> on <b>" + results.currentHostname + "</b>:";
+    }
+    document.getElementById("hostnameIncorrect-p").innerHTML = message;
     // for (let i in results[results.currentHostname]["scriptTags"][results.currentPathname]) {
     //     addElement("currentScriptTag", i, "correct");
     // }
